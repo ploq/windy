6ffd5928d13b54944a8727f1e359eeb5597622d7
Marcus Gustafsson <margu478@student.liu.se>
2016-10-15 18:10:44 +0200

    Static files are now served by Express

--- pubserver.js
+++ pubserver.js
@@ -3,6 +3,11 @@ var app = express();
 var api = require ("./routes/api");
 
 app.use("/api", api);
+app.use('/static', express.static('static'));
 
+app.get("/", function(req, res){
+    res.sendfile("static/main.html");
+});
 
 app.listen(8080, function () {});
+
